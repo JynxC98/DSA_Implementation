819b70ba3d1d5147aa0379b03d9a873aa30c928e
Harsh Parikh <parikh.harsh.hp@gmail.com>
2024-08-16 17:30:38 +0530

    added the code for double linked list

--- python/data_structures/linked_list/double_linked_list.py
+++ python/data_structures/linked_list/double_linked_list.py
@@ -1,122 +1,167 @@
 """
-Python implementation of a double linked list
+Implementation of double linked list.
 """
 
-
-# Defining the node class
 class Node:
+    """
+    A class representing a node in a doubly linked list.
+
+    Attributes:
+        value: The value stored in the node.
+        next: Reference to the next node in the list.
+        prev: Reference to the previous node in the list.
+    """
+
     def __init__(self, value):
         self.value = value
         self.next = None
         self.prev = None
-        
 
 
 class DoubleLinkedList:
+    """
+    A class implementing a doubly linked list data structure.
+
+    Attributes:
+        head: Reference to the first node in the list.
+        tail: Reference to the last node in the list.
+    """
+
     def __init__(self):
         self.head = None
         self.tail = None
-        
-        
+
     def insert_element(self, value, index):
+        """
+        Insert a new element at the specified index in the list.
+
+        Args:
+            value: The value to be inserted.
+            index (int): The position at which to insert the new element.
+
+        Returns:
+            str: "Not possible" if the insertion is not possible at the given index.
+        """
         new_node = Node(value)
-        
-        # case 1: When the list is empty
-        
-        if self.head is None:
+
+        # Case 1: If the head is empty
+        if not self.head:
             self.head = new_node
             self.tail = new_node
             return
-        
-            
-        # Case 2: We are replacing the head value
+        # Case 2: Replacing the head with a new element
         if index == 0:
             new_node.next = self.head
             self.head = new_node
-            self.head.prev = None
+            self.head.next.prev = self.head
             return
-        
+
         else:
             current_node = self.head
-            
-            # Since the indexation starts from 0
-            for _ in range(index - 1):
-                if not current_node.next:
-                    return "Not enough elements"
-                    
+
+            for _ in range(1, index):
+                if not current_node:
+                    return "Not possible"
+
                 current_node = current_node.next
-            
-        # Case 3: We are adding it to the end
-            if current_node.next is None:
+
+        # Case 3: We are adding the node at the end
+            if not current_node.next:
                 current_node.next = new_node
                 new_node.prev = current_node
                 self.tail = new_node
-                
                 return
-            
-        # Case 4: We are adding it in the middle
-        
-            next_node = current_node.next
-            current_node.next = new_node
-            new_node.prev = current_node
-            new_node.next = next_node
-            next_node.prev = new_node
-            
-            return
-        
+
+        # Case 4: We are adding the node in the middle
+        next_node = current_node.next
+        current_node.next = new_node
+        new_node.prev = current_node
+        new_node.next = next_node
+        next_node.prev = new_node
+        return
+
     def remove_element(self, index):
-        
-        current_node = self.head
-        # Case 1: We are removing the head
+        """
+        Remove an element at the specified index from the list.
+
+        Args:
+            index (int): The position of the element to be removed.
+
+        Returns:
+            str: "No elements remaining" if the list is empty.
+                 "Not possible" if the removal is not possible at the given index.
+        """
+        # Case 1: There are no elements present
+        if not self.head:
+            return "No elements remaining"
+
+        # Case 2: We are removing the head
         if index == 0:
-            next_node = current_node.next
+            temp = self.head
+            next_node = temp.next
             self.head = next_node
-            del current_node
+
+            del temp
+            self.head.prev = None
             return
-        
-        for _ in range(index - 1):
-            if not current_node:
+
+        current_node = self.head
+
+        # Since we need to remove the nth index, we use `index+1`
+        for _ in range(1, index + 1):
+            if not current_node.next:
                 return "Not possible"
             current_node = current_node.next
-            
-        
-        # Case 2: We are removing the tail
-        if current_node.next is None:
+
+        # Case 3: Removing the tail element
+        if not current_node.next:
             prev_node = current_node.prev
             self.tail = prev_node
+            self.tail.next = None
             del current_node
             return
-        
-        # Case 3: We are removing an element from the middle
-        next_node = current_node.next
+
         prev_node = current_node.prev
-        
+        next_node = current_node.next
+
         prev_node.next = next_node
         next_node.prev = prev_node
         del current_node
         return
-    
+
     def traverse_forward(self):
+        """
+        Traverse the list from head to tail and return a string representation.
+
+        Returns:
+            str: A string representation of the list elements connected by " <-> ".
+        """
         result = ""
-        
+
         current_node = self.head
-        
+
         while current_node:
             result += f"{current_node.value} <-> " if current_node.next is not None else f"{current_node.value}"
             current_node = current_node.next
-            
+
         return result
-        
+
     def traverse_backward(self):
+        """
+        Traverse the list from tail to head and return a string representation.
+
+        Returns:
+            str: A string representation of the list elements connected by " <-> ".
+        """
         result = ""
         current_node = self.tail
-        
+
         while current_node:
             result += f"{current_node.value} <-> " if current_node.prev is not None else f"{current_node.value}"
             current_node = current_node.prev
-            
+
         return result
-        
+
 
 if __name__ == "__main__":
     double_ll = DoubleLinkedList()
@@ -124,25 +169,13 @@ if __name__ == "__main__":
     double_ll.insert_element(2, 1)
     double_ll.insert_element(3, 2)
     double_ll.insert_element(4, 3)
-    
     print(double_ll.traverse_forward())
-    double_ll.remove_element(4)
     print(double_ll.traverse_backward())
-            
-            
-        
-            
-            
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
+    double_ll.insert_element("Hello", 4)
+    print(double_ll.traverse_forward())
+
+    double_ll.remove_element(4)
+    print(double_ll.traverse_forward())
+    double_ll.remove_element(0)
+    print(double_ll.traverse_forward())
+    print(double_ll.traverse_backward())
